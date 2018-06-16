8a6605f0ac823621c77b6d6d8405dcfcdd8ae6a3
eqinex <oxxolinecs@mail.ru>
2018-06-16 15:51:28 +0500

    start

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Pizza</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="heading clearfix">
+                <img src="img/logotype.png" alt="logotype" class="logo">
+                <nav>
+
+                </nav>
+            </div>
+        </div>
+    </header>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
